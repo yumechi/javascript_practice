49995b3fe4b716e56956a9f70ae9c016b630d875
yumechi <yumechi0525@gmail.com>
2017-10-03 19:14:11 +0900

    get filename

--- chrome_plugin_practice/downloaderApp/app/scripts.babel/contentscript.js
+++ chrome_plugin_practice/downloaderApp/app/scripts.babel/contentscript.js
@@ -5,10 +5,15 @@ $(function () {
 });
 
 $(document).on('click', '#slack_emoji_dl', function () {
+    
     $('html').html().split('\n').some(function(val, index) {
-        if(!val.match(/data-original=\".*?\"/)) {
+        if(!val.match(/data-original=\"(.*?)\"/)) {
             return false;
         }
-        console.log(val);
+        let url = val.match(/data-original=\"(.*?)\"/)[1];
+        let fragments = url.split('/');
+        let filename = fragments[fragments.length-2] + 
+            '.' + fragments[fragments.length-1].split('.')[1];
+        console.log(filename);
     });
 });
