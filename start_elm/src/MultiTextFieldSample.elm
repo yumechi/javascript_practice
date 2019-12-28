d67d3730dbd543c069720c2405723d875d1dbb06
yumechi <yumechi0525@gmail.com>
2019-12-28 23:46:01 +0900

    validations drill

--- start_elm/src/MultiTextFieldSample.elm
+++ start_elm/src/MultiTextFieldSample.elm
@@ -20,6 +20,7 @@ main =
 
 type alias Model =
     { name : String
+    , age : String
     , password : String
     , passwordAgain : String
     }
@@ -27,7 +28,7 @@ type alias Model =
 
 init : Model
 init =
-    Model "" "" ""
+    Model "" "" "" ""
 
 
 
@@ -36,6 +37,7 @@ init =
 
 type Msg
     = Name String
+    | Age String
     | Password String
     | PasswordAgain String
 
@@ -45,6 +47,9 @@ update msg model =
     case msg of
         Name name ->
             { model | name = name }
+
+        Age age ->
+            { model | age = age }
 
         Password password ->
             { model | password = password }
@@ -61,9 +66,11 @@ view : Model -> Html Msg
 view model =
     div []
         [ viewInput "text" "Name" model.name Name
+        , viewInput "age" "Age" model.age Age
         , viewInput "password" "Password" model.password Password
         , viewInput "passwordAgain" "Re-enter Password" model.passwordAgain PasswordAgain
-        , viewValidation model
+        , ageValidation model
+        , passwordValidation model
         ]
 
 
@@ -72,13 +79,58 @@ viewInput t p v toMsg =
     input [ type_ t, placeholder p, value v, onInput toMsg ] []
 
 
-viewValidation : Model -> Html msg
-viewValidation model =
-    if model.password == "" && model.passwordAgain == "" then
+ageValidation : Model -> Html msg
+ageValidation model =
+    if String.all Char.isDigit model.age || isEmptyAge model then
         div [ style "color" "white" ] [ text "" ]
 
-    else if model.password == model.passwordAgain then
+    else
+        div [ style "color" "red" ] [ text "Illegal AgeInput, remove not digit character" ]
+
+
+passwordValidation : Model -> Html msg
+passwordValidation model =
+    if isEmptyPassword model then
+        div [ style "color" "white" ] [ text "" ]
+
+    else if isShortPassword model then
+        div [ style "color" "red" ] [ text "Too Short Password, please input over 8 character" ]
+
+    else if isHavingPattern model then
+        div [ style "color" "red" ] [ text "Password needs to having lower alphabets, upper alphabets and digits" ]
+
+    else if isEqualPassword model then
         div [ style "color" "green" ] [ text "OK" ]
 
     else
         div [ style "color" "red" ] [ text "Password not match" ]
+
+
+isEmptyInput : String -> Bool
+isEmptyInput line =
+    String.length line == 0 && String.length line == 0
+
+
+isEmptyAge : Model -> Bool
+isEmptyAge model =
+    isEmptyInput model.age
+
+
+isEmptyPassword : Model -> Bool
+isEmptyPassword model =
+    isEmptyInput model.password
+
+
+isEqualPassword : Model -> Bool
+isEqualPassword model =
+    model.password == model.passwordAgain
+
+
+isShortPassword : Model -> Bool
+isShortPassword model =
+    String.length model.password < 8
+
+
+isHavingPattern : Model -> Bool
+isHavingPattern model =
+    not (String.any Char.isDigit model.password && String.any Char.isLower model.password && String.any Char.isUpper model.password)
